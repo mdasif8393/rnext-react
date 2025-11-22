591babc5f588dede02037ea88b8286df149e36af
mdasif8393 <md.asif.8393@gmail.com>
2025-11-22 22:31:53 +0600

    complete 1.4 - Basics of React Components: Your first component

---
+++ module-1-getting-started-with-react-describing-the-ui/1.4-basics-of-react-component-your-first-component/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Gallery from "./components/Gallery";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Gallery />
+    </>
+  );
+}
+
+export default App;
