7a2fa4f3a8f35085234e7312c97d2ac2290cfac2
mdasif8393 <md.asif.8393@gmail.com>
2025-11-23 12:35:06 +0600

    1.5 - Basics of React Components: Importing & Exporting Components

---
+++ module-1-getting-started-with-react-describing-the-ui/1.5-basics-of-react-components-import-and-export-components/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import G from "./components/Gallery";
+import { Profile as Image } from "./components/Gallery";
+
+function App() {
+  return (
+    <>
+      <Image />
+      <G />
+    </>
+  );
+}
+
+export default App;
