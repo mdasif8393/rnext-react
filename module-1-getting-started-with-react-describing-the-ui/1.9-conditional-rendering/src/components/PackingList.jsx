ce0cea317aaff7d71d5f9fcab5d4602d3554acf0
mdasif8393 <md.asif.8393@gmail.com>
2025-11-25 15:31:23 +0600

    conditional rendering

---
+++ module-1-getting-started-with-react-describing-the-ui/1.9-conditional-rendering/src/components/PackingList.jsx
@@ -0,0 +1,22 @@
+function Item({ name, isPacked }) {
+  let itemContent = name;
+
+  if (isPacked) {
+    itemContent = <del> name + "✅"</del>;
+  }
+
+  return <li className="item">{itemContent}</li>;
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={false} name="Helmet with a golden leaf" />
+        <Item isPacked={true} name="hoto of Tam" />
+      </ul>
+    </section>
+  );
+}
