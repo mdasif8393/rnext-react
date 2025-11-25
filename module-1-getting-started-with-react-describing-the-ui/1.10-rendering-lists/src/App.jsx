1ec2d52406ebe3ad89f767c6ce3db04ee56e4b68
mdasif8393 <md.asif.8393@gmail.com>
2025-11-25 18:19:23 +0600

    rendering list

---
+++ module-1-getting-started-with-react-describing-the-ui/1.10-rendering-lists/src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+
+const people = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+  },
+];
+
+function App() {
+  const data = people.filter((x) => x.profession === "chemist");
+  const listItems = data.map((x) => <li key={data.id}>{x.name}</li>);
+
+  return <>{<ul>{listItems}</ul>}</>;
+}
+
+export default App;
