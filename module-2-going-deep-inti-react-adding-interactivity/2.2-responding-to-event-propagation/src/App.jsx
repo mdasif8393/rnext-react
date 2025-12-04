cc02d7231d25f1685b8f967ba4631e0b13d5345e
mdasif8393 <md.asif.8393@gmail.com>
2025-12-04 16:52:09 +0600

    2.2 Responding to Events - Event Propagation

---
+++ module-2-going-deep-inti-react-adding-interactivity/2.2-responding-to-event-propagation/src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function Button({ onClick, children }) {
+  return (
+    <button
+      onClick={(e) => {
+        e.stopPropagation();
+        onClick();
+      }}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default function App() {
+  return (
+    <div
+      className="Toolbar"
+      onClick={() => {
+        console.log("You clicked on the toolbar!");
+      }}
+    >
+      <Button onClick={() => console.log("Playing!")}>Play Movie</Button>
+      <Button onClick={() => console.log("Uploading!")}>Upload Image</Button>
+    </div>
+  );
+}
